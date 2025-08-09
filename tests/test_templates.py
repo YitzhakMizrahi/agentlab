81f05dce0e9be94c63f05ae95ef3ea8a8d2d594f
YitzhakMizrahi <mizrahi.yitzchak@gmail.com>
2025-08-10 00:13:09 +0300

    test(tests): add unit tests for template rendering functions

---
+++ tests/test_templates.py
@@ -0,0 +1,17 @@
+from agentlab.utils.templates import render_template, render_mapping
+
+
+def test_render_template_basic():
+    assert render_template("Hello {{name}}", {"name": "World"}) == "Hello World"
+
+
+def test_render_template_missing_kept():
+    assert render_template("{{unknown}}", {}) == "{{unknown}}"
+
+
+def test_render_mapping_nested():
+    data = {"a": "{{x}}", "b": {"c": "{{y}}"}, "d": ["{{x}}", 1, {"e": "{{y}}"}]}
+    out = render_mapping(data, {"x": "X", "y": "Y"})
+    assert out == {"a": "X", "b": {"c": "Y"}, "d": ["X", 1, {"e": "Y"}]}
+
+
